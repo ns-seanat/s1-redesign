5d3409bbed00a709f91a15e2f16eea68b7ac2a7c
CatgirlCultist <44424202+CatgirlCultist@users.noreply.github.com>
2021-08-06 22:20:04 -0400

    Create userscript.js

---
+++ userscript.js
@@ -0,0 +1,24 @@
+// ==UserScript==
+// @name         S1 Redesign
+// @namespace    http://tampermonkey.net/
+// @version      0.1
+// @description  Makes the flag larger on S1 and removes the infobox
+// @author       Seanat (aka Yuuka, CatgirlCultist, cbp105)
+// @match        *www.nationstates.net/*
+// @icon         https://www.google.com/s2/favicons?domain=nationstates.net
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    var s1Cards = document.getElementsByClassName("deckcard deckcard-season-1 ");
+
+    for(var deckcard of s1Cards) {
+        var flagElement = deckcard.getElementsByClassName("deckcard-flag")[0];
+        flagElement.style.height = "180px";
+        var descBoxElement = deckcard.getElementsByClassName("deckcard-desc")[0];
+        descBoxElement.remove();
+    }
+
+})();
